152fb373e2dec5c50fc41724a09518ba1cf589d0
trollglodita <1trollglodita@gmail.com>
2013-11-06 13:07:44 -0300

    HTTP response middleware

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(function (req, res, next) {
+	res.set('X-Powered-By', 'Flight Tracker');
+	next();
+});
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
