c4403cdedf704f18a3e84568aad86337bceeafe8
trollglodita <1trollglodita@gmail.com>
2013-11-13 01:19:12 -0300

    Isolating code, helper functions and Unit testing Mocha

--- app.js
+++ app.js
@@ -3,40 +3,39 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var http = require('http');
-var path = require('path');
 
-var app = express();
+module.exports = function (flights) {
+	var express = require('express');
+	var routes = require('./routes')(flights);
+	var path = require('path');
+	var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(function (req, res, next) {
-	res.set('X-Powered-By', 'Flight Tracker');
-	next();
-});
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+	// all environments
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(express.methodOverride());
+	app.use(function (req, res, next) {
+		res.set('X-Powered-By', 'Flight Tracker');
+		next();
+	});
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	// development only
+	if ('development' == app.get('env')) {
+	  app.use(express.errorHandler());
+	}
+
+	app.get('/flight/:number', routes.flight);
+	app.put('/flight/:number/arrived', routes.arrived);
+	app.get('/list', routes.list);
+	app.get('/list/json', routes.listjson);
+
+	return app;
 }
 
-app.get('/flight/:number', routes.flight);
-app.put('/flight/:number/arrived', routes.arrived);
-app.get('/list', routes.list);
-app.get('/list/json', routes.listjson);
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
---
+++ test/data/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+	"12": {
+		"number": 12,
+		"origin": 'AUS',
+		"destination": 'DCA',
+		"departs": '10:00 AM',
+		"arrives": '12:00 AM'
+	},
+	"13": {
+		"number": 13,
+		"origin": 'AUS',
+		"destination": 'CLS',
+		"departs": '10:00 AM',
+		"arrives": '12:00 AM'
+	},
+	"14": {
+		"number": 14,
+		"origin": 'AUS',
+		"destination": 'CHN',
+		"departs": '10:00 AM',
+		"arrives": '12:00 AM'
+	},
+}
