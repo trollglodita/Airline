86e2b153c264cda31a111ecec9158e8e5cbbd3fd
trollglodita <1trollglodita@gmail.com>
2013-12-18 08:48:58 -0300

    Repl Argv added

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+#!/usr/bin/env node
+
 var http = require('http'),
 	flights = require('./data'),
 	db = require('./db'),
-	app = require('./app')(flights);
+	argv = require('optimist').argv,
+	app = require('./app')(flights, db);
 
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+if (argv.flight && argv.destination) {
+	flights[argv.flight].data.destination = argv.destination;
+}
